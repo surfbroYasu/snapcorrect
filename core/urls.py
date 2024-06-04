2dcdbf9a84d83622563ab97efeb0f2c507004441
Yasufumi Misono <yasufumimisono@gmail.com>
2024-06-04 09:03:57 +0900

    Add google search console varification file

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,13 @@
-from django.urls import path 
+from django.urls import path, re_path
+from django.views.generic import TemplateView
 from . import views
+
 
 app_name = "core"
 
 urlpatterns = [
     path("", views.Homepage.as_view(), name= "home"),
     path("about/", views.About.as_view(), name= "about"),
+
+    re_path(r'^google0e9665c9287f6795\.html$', TemplateView.as_view(template_name="google0e9665c9287f6795.html", content_type='text/html')),
 ]
