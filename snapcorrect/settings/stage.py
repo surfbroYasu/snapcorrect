5036f280ca7ff6483a5409d3269e8b6f0950d809
Yasufumi Misono <yasufumimisono@gmail.com>
2024-06-06 18:21:21 +0900

    configure staging setting

--- snapcorrect/settings/stage.py
+++ snapcorrect/settings/stage.py
@@ -5,8 +5,17 @@ from dotenv import load_dotenv
 load_dotenv()
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
-DEBUG = os.environ.get("DEBUG")
-PRODUCTION = os.environ.get("PRODUCTION")
+
+DEBUG = str(os.environ.get("DEBUG"))=="0" #1 being True, 
+# Always KEEP DEBUG = False in production!!!
+
+INSTALLED_APPS += ['debug_toolbar', ]
+
+MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware', ]
+
+PRODUCTION = str(os.environ.get("PRODUCTION"))=="0" #1 being True
+# This is a dev setting defult PRODUCTION = "0" being false
+
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 
@@ -34,13 +43,46 @@ DATABASES = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATICFILES_DIR = [
-    os.path.join(BASE_DIR, "static"),
-    os.path.join(BASE_DIR, "media"),
-]
 
 STATIC_URL = "/static/"
 MEDIA_URL = "/media/"
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    # BASE_DIR / "media",
+]
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiels')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# AMAZON CONFIG
+
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+
+AWS_STORAGE_BUCKET_NAME = 'snapcorrect'
+
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+AWS_S3_FILE_OVERWRITE = False
+
+STORAGES = {
+    # media file management
+    "default": {
+        "BACKEND": "storages.backends.s3boto3.S3StaticStorage"
+        },
+    # CSS and JS file management
+    "staticfiles": {
+        "BACKEND": "storages.backends.s3boto3.S3StaticStorage"
+        },
+}
+
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = os.environ.get("EMAIL_ADDRESS")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'SnapCorrect'
+ACCOUNT_EMAIL_PREFIX = ''
