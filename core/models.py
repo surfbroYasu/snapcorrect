9f90abc0df8a489975e5c6fe365f2c54e06a9fc4
Yasufumi Misono <yasufumimisono@gmail.com>
2024-06-09 14:23:51 +0900

    Created a new school table and added a foreign key to user -migration made in dev DB

--- core/models.py
+++ core/models.py
@@ -4,6 +4,20 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+
+class School(models.Model):
+    name = models.CharField(
+        max_length=75
+    )
+    phone = models.TextField(
+        null=True,
+        blank=True
+    )
+    address = models.TextField(
+        max_length=125
+    )
+    
+
 class Profile(models.Model):
     user = models.OneToOneField(
         User,
@@ -13,7 +27,16 @@ class Profile(models.Model):
     birthday = models.DateField( 
         null=True
         )
-
+    # teacher status extension
+    is_teacher = models.BooleanField(
+        default=False
+    )
+    organization = models.ForeignKey(
+        School,
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL
+    )
     
     def __str__(self):
         return self.user.username
